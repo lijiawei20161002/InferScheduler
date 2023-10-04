1ba64a1f00d5d3536f98ac899c19bf3ef684b1a7
李佳蔚 <lijiawei@DESKTOP-NHQ2VP5.fareast.corp.microsoft.com>
2023-10-04 22:34:43 +0800

    support scheduling policy

--- scheduler.py
+++ scheduler.py
@@ -1,22 +1,24 @@
-import argparse
+import argparse  
 import random  
   
   
 class Request:  
-    def __init__(self, tokens, deadline):  
+    def __init__(self, tokens, deadline, arrival_time):  
         self.tokens = tokens  
         self.deadline = deadline  
-  
-    self.score = tokens / deadline  
+        self.arrival_time = arrival_time  
+        self.score = tokens / deadline  
   
     def update_score(self, current_time):  
-        self.score = self.tokens / (self.deadline - current_time)   
+        self.score = self.tokens / (self.deadline - current_time)  
   
   
 class SchedulerSimulator:  
-    def __init__(self, requests, inference_delays):  
-        self.requests = requests 
+    def __init__(self, requests, inference_delays, policy):  
+        self.requests = requests  
         self.inference_delays = inference_delays  
+        self.current_time = 0  
+        self.policy = policy
   
     def calculate_delay(self, batch_size):  
         if batch_size in self.inference_delays:  
@@ -25,11 +27,17 @@ class SchedulerSimulator:
             return float('inf')  # Assume infinite delay for batch sizes not in the list  
   
     def scheduler(self, processing_requests):  
-        selected_requests = random.sample(processing_requests, min(len(processing_requests), 16))  
-        batch_size = max(len(selected_requests), 16)  # Ensure batch size is no smaller than the number of selected requests  
+        if self.policy == 'bidding':
+            for req in processing_requests:  
+                req.update_score(self.current_time)  
+            selected_requests = sorted(processing_requests, key=lambda req: req.score, reverse=True)[:16]  
+            batch_size = max(len(selected_requests), 16)  # Ensure batch size is no smaller than the number of selected requests  
+        if self.policy == 'random':
+            selected_requests = random.sample(processing_requests, min(len(processing_requests), 16))  
+            batch_size = max(len(selected_requests), 16)  # Ensure batch size is no smaller than the number of selected requests  
         return selected_requests, batch_size  
   
-    def run_one_iteration(self, processing_requests, current_time, goodput):  
+    def run_one_iteration(self, processing_requests, goodput):  
         total_delay_now = 0  
   
         selected_requests, batch_size = self.scheduler(processing_requests)  
@@ -38,7 +46,7 @@ class SchedulerSimulator:
             req.tokens -= 1  
             if req.tokens == 0:  
                 processing_requests.remove(req)  
-                if current_time <= req.deadline:  
+                if self.current_time <= req.deadline:  
                     goodput += 1  # Increment goodput if request finishes before deadline  
             delay = self.calculate_delay(batch_size)  
             total_delay_now += delay  
@@ -47,12 +55,15 @@ class SchedulerSimulator:
   
     def run_simulation(self):  
         goodput = 0  
-        current_time = 0  
-        processing_requests = self.requests.copy()  
+        processing_requests = []  
   
-        while processing_requests:  
-            current_time += 1  
-            _, goodput = self.run_one_iteration(processing_requests, current_time, goodput)  
+        while self.requests or processing_requests:  
+            self.current_time += 1  
+            arrived_requests = [req for req in self.requests if req.arrival_time <= self.current_time]  
+            processing_requests.extend(arrived_requests)  
+            self.requests = [req for req in self.requests if req not in arrived_requests]  
+  
+            _, goodput = self.run_one_iteration(processing_requests, goodput)  
   
         return goodput  
   
@@ -62,12 +73,13 @@ class SchedulerSimulator:
             Request(  
                 tokens := random.randint(min_tokens, max_tokens),  
                 deadline=inference_delays[1] * tokens + int(random.expovariate(lambda_param)),  
+                arrival_time=round(random.uniform(0, num_requests))  
             )  
             for _ in range(num_requests)  
         ]  
         return requests  
- 
- 
+  
+  
 if __name__ == "__main__":  
     random.seed(42)  
   
@@ -77,19 +89,22 @@ if __name__ == "__main__":
     num_requests = 1000  
     min_tokens = 10  
     max_tokens = 100  
-
+  
     parser = argparse.ArgumentParser(description="Scheduler Simulator")  
     # Add arguments  
-    parser.add_argument("--num_requests", type=int, default=1000, help="Number of requests to generate")  
+    parser.add_argument("--num_requests", type=int, default=1000, help="Number of requests to generate") 
+    parser.add_argument("--policy", type=str, default="random", help="Specify scheduling policy")   
     # Parse the arguments  
     args = parser.parse_args()  
     # Access the parsed arguments  
-    num_requests = args.num_requests
-
-    simulator = SchedulerSimulator([], inference_delays)  
+    num_requests = args.num_requests  
+    policy = args.policy
+  
+    simulator = SchedulerSimulator([], inference_delays, policy)  
     requests = simulator.generate_requests(num_requests, min_tokens, max_tokens, inference_delays)  
     simulator.requests = requests  
   
     goodput = simulator.run_simulation()  
   
     print(f"Goodput: {goodput}")  
+
