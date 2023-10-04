8f7db985893ca480e94eb3a411fb13d7b7f9640b
李佳蔚 <lijiawei@DESKTOP-NHQ2VP5.fareast.corp.microsoft.com>
2023-10-04 22:34:43 +0800

    support scheduling policy

--- run_scheduler.py
+++ run_scheduler.py
@@ -2,18 +2,18 @@ import os
 import sys  
 import matplotlib.pyplot as plt  
 from scheduler import SchedulerSimulator  
-
+  
 inference_delays = {1: 42.89945313, 2: 45.02945313, 4: 50.47695313, 8: 62.123125, 16: 84.1871875}  
   
-def main(num_requests, min_tokens, max_tokens, inference_delays):  
+def main(num_requests, min_tokens, max_tokens, inference_delays, policy):  
     # Initialize the scheduler simulator  
-    simulator = SchedulerSimulator([], inference_delays)  
+    simulator = SchedulerSimulator([], inference_delays, policy)  
   
     # Generate the requests  
     requests = simulator.generate_requests(num_requests, min_tokens, max_tokens, inference_delays)  
-    simulator.requests = requests
-
-    # Run the simulation   
+    simulator.requests = requests  
+  
+    # Run the simulation  
     goodput = simulator.run_simulation()  
   
     # Return the goodput  
@@ -24,17 +24,25 @@ if __name__ == "__main__":
     max_tokens = 100  
   
     num_requests_values = list(range(100, 2100, 100))  
-    goodput_values = []  
+    goodput_values_random = []  
+    goodput_values_bidding = []  
   
-    # Loop through num_requests from 100 to 2000 with a step of 100  
+    # Loop through num_requests from 100 to 2000 with a step of 100 for random policy  
     for num_requests in num_requests_values:  
-        goodput = main(num_requests, min_tokens, max_tokens, inference_delays)  
-        goodput_values.append(goodput)  
+        goodput_random = main(num_requests, min_tokens, max_tokens, inference_delays, policy='random')  
+        goodput_values_random.append(goodput_random)  
+  
+    # Loop through num_requests from 100 to 2000 with a step of 100 for bidding policy  
+    for num_requests in num_requests_values:  
+        goodput_bidding = main(num_requests, min_tokens, max_tokens, inference_delays, policy='bidding')  
+        goodput_values_bidding.append(goodput_bidding)  
   
     # Plot the results  
-    plt.plot(num_requests_values, goodput_values, marker='o', linestyle='-', linewidth=2)  
+    plt.plot(num_requests_values, goodput_values_random, marker='o', linestyle='-', linewidth=2, label='Random')  
+    plt.plot(num_requests_values, goodput_values_bidding, marker='o', linestyle='-', linewidth=2, label='Bidding')  
     plt.xlabel("Number of Requests")  
     plt.ylabel("Goodput")  
     plt.title("Scheduler Simulator Goodput vs. Number of Requests")  
+    plt.legend()  
     plt.grid()  
-    plt.savefig("test.png")
+    plt.savefig("test.png")  
