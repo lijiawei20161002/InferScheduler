fbffe7e094f6ee9ea7917c8509b02574e9b52744
李佳蔚 <lijiawei@MININT-Q3T9AM9.fareast.corp.microsoft.com>
2023-10-05 08:40:08 +0800

    fix error in delay calculation

--- run_scheduler.py
+++ run_scheduler.py
@@ -5,12 +5,12 @@ from scheduler import SchedulerSimulator
   
 inference_delays = {1: 42.89945313, 2: 45.02945313, 4: 50.47695313, 8: 62.123125, 16: 84.1871875}  
   
-def main(num_requests, min_tokens, max_tokens, inference_delays, policy):  
+def main(num_requests, inference_delays, policy):  
     # Initialize the scheduler simulator  
     simulator = SchedulerSimulator([], inference_delays, policy)  
   
     # Generate the requests  
-    requests = simulator.generate_requests(num_requests, min_tokens, max_tokens, inference_delays)  
+    requests = simulator.generate_requests(num_requests, inference_delays)  
     simulator.requests = requests  
   
     # Run the simulation  
@@ -19,22 +19,19 @@ def main(num_requests, min_tokens, max_tokens, inference_delays, policy):
     # Return the goodput  
     return goodput  
   
-if __name__ == "__main__":  
-    min_tokens = 1  
-    max_tokens = 100  
-  
+if __name__ == "__main__":   
     num_requests_values = list(range(100, 2100, 100))  
     goodput_values_random = []  
     goodput_values_bidding = []  
   
     # Loop through num_requests from 100 to 2000 with a step of 100 for random policy  
     for num_requests in num_requests_values:  
-        goodput_random = main(num_requests, min_tokens, max_tokens, inference_delays, policy='random')  
+        goodput_random = main(num_requests, inference_delays, policy='random')  
         goodput_values_random.append(goodput_random)  
   
     # Loop through num_requests from 100 to 2000 with a step of 100 for bidding policy  
     for num_requests in num_requests_values:  
-        goodput_bidding = main(num_requests, min_tokens, max_tokens, inference_delays, policy='bidding')  
+        goodput_bidding = main(num_requests, inference_delays, policy='bidding')  
         goodput_values_bidding.append(goodput_bidding)  
   
     # Plot the results  
