5b261630dbf0cbbf1560c2ada918d65d0d4d748e
Ubuntu <ubuntu@ip-172-31-64-78.ec2.internal>
2023-11-23 16:35:48 +0000

    test

--- run_scheduler.py
+++ run_scheduler.py
@@ -1,25 +1,21 @@
 import matplotlib.pyplot as plt  
 from scheduler import SchedulerSimulator  
+import copy
+import os
   
 inference_delays = {1: 42.89945313, 2: 45.02945313, 4: 50.47695313, 8: 62.123125, 16: 84.1871875}  
   
-def main(num_requests, inference_delays, scheduling_policy, batching_policy):  
-    # Initialize the scheduler simulator  
-    simulator = SchedulerSimulator([], inference_delays, scheduling_policy, batching_policy)  
-  
-    # Generate the requests  
-    requests = simulator.generate_requests(num_requests, inference_delays)  
+def main(simulator, requests):    
     simulator.requests = requests  
-    if scheduling_policy == 'offline optimal':
-        simulator.calculate_offline_optimal()
-  
+    if scheduling_policy == 'offline solver':
+        simulator.call_offline_solver()
+
     # Run the simulation  
     goodput, average_jct = simulator.run_simulation()  
-    simulator.reset([], inference_delays, scheduling_policy, batching_policy)
-    del simulator
-  
-    # Return the goodput and average_jct  
-    return goodput, average_jct  
+    objective_metric = simulator.calculate_objective_from_log(f'{scheduling_policy}.log', scheduling_policy)
+
+    # Return the goodput, average_jct and objective_metric
+    return goodput, average_jct, objective_metric  
   
 def plot_results(x_values, y_values, xlabel, ylabel, title, filename):  
     for policy, values in y_values.items():  
@@ -41,35 +37,46 @@ if __name__ == "__main__":
   
     # Define colors for each scheduling policy  
     color_map = {  
-        'offline optimal': 'purple',  
-        'repeated offline solver': 'pink',
+        'offline solver': 'purple',  
+        'online solver': 'pink',
         'random': 'blue',  
         'bidding': 'green',  
         'fcfs': 'red',  
         'deadline': 'orange'  
     }  
   
-    scheduling_policies = ['offline optimal', 'repeated offline solver', 'random', 'bidding', 'fcfs', 'deadline']  
+    scheduling_policies = ['offline solver', 'online solver', 'random', 'bidding', 'fcfs', 'deadline']  
     batching_policies = [16]  
   
     goodput_values = {}  
     average_jct_values = {}  
+    objective_metrics = {}
     for policy in scheduling_policies:  
         for batch_policy in batching_policies:  
-            key = f'{policy}_{"wo" if batch_policy == 16 else "w"}_dynamic_batching'  
+            key = f'{policy}'  
             goodput_values[key] = []  
             average_jct_values[key] = []  
+            objective_metrics[key] = []
   
+    # Initialize the scheduler simulator  
+    simulator = SchedulerSimulator([], inference_delays, 'offline solver', 16)
+
     # Loop through num_requests from 100 to 2000 with a step of 100 for random policy  
     for num_requests in num_requests_values:  
-        for policy in scheduling_policies:  
+        # Generate the requests  
+        original_requests = simulator.generate_requests(num_requests, inference_delays)
+        for scheduling_policy in scheduling_policies:  
             for batch_policy in batching_policies:  
-                print(policy, batch_policy)
-                goodput, average_jct = main(num_requests, inference_delays, scheduling_policy=policy, batching_policy=batch_policy)  
-                key = f'{policy}_{"wo" if batch_policy == 16 else "w"}_dynamic_batching'  
+                print(scheduling_policy, batch_policy)  
+                requests = copy.deepcopy(original_requests)
+                simulator.reset([], inference_delays, scheduling_policy, batch_policy)
+                goodput, average_jct, objective_metric = main(simulator, requests)  
+                key = f'{scheduling_policy}'  
                 goodput_values[key].append(goodput)  
                 average_jct_values[key].append(average_jct)  
-  
+                objective_metrics[key].append(objective_metric)
+                #os.system('python3 autoremove.py')
+    
     # Plot the goodput results  
     plot_results(num_requests_values, goodput_values, "Number of Requests", "Goodput",  
                  "Scheduler Simulator Goodput vs. Number of Requests", "goodput.png")  
@@ -77,3 +84,8 @@ if __name__ == "__main__":
     # Plot the average JCT results  
     plot_results(num_requests_values, average_jct_values, "Number of Requests", "Average JCT",  
                  "Scheduler Simulator Average JCT vs. Number of Requests", "average_jct.png")  
+
+    # Plot the goodput results  
+    plot_results(num_requests_values, objective_metrics, "Number of Requests", "Objective",  
+                 "Scheduler Simulator Objective vs. Number of Requests", "objective.png")  
+  
