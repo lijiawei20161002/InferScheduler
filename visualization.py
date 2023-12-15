84966b8f7b7dabecfde70497616af210b6721b37
Jiawei Li <jiaweil7@catalyst-cluster.cs.cmu.edu>
2023-12-15 11:21:04 -0500

    change request_generation

--- visualization.py
+++ visualization.py
@@ -35,16 +35,21 @@ def parse_request_data(filename, request_ids, request_num):
 
     return request_data
 
-def parse_request_num(filename):
+def parse_request_num(filename, request_num):
     with open(filename, 'r') as file:
         log_data = file.read()
-    iteration_pattern = r"Iteration: (\d+), Time:.*?Current Requests: \[(.*?)\]"
-    iterations = re.findall(iteration_pattern, log_data, re.DOTALL)
-    current_request_counts = []
+    # Pattern to capture each group starting with 'N=XX'
+    group_pattern = r"N=(\d+)\n-+\n(.*?)(?=\n-+\nN=\d+|$)"
+    groups = re.findall(group_pattern, log_data, re.DOTALL)
+    for n_value, group_data in groups:
+        if int(n_value) == request_num:
+            iteration_pattern = r"Iteration: (\d+), Time:.*?Current Requests: \[(.*?)\]"
+            iterations = re.findall(iteration_pattern, group_data, re.DOTALL)
+            current_request_counts = []
 
-    for iteration, requests in iterations:
-        current_requests = requests.split('}, {') if requests else []
-        current_request_counts.append(len(current_requests))
+            for iteration, requests in iterations:
+                current_requests = requests.split('}, {') if requests else []
+                current_request_counts.append(len(current_requests))
 
     return current_request_counts   
 
@@ -78,15 +83,14 @@ def plot_request_counts(request_counts, filename):
     plt.grid(True)
     plt.savefig(filename)
 
-
+'''
 logfile = 'offline solver.log'  # Replace with your log file path
 request_ids = ['10', '12', '3', '4', '5', '6', '7', '8', '9']  # Replace with your list of request IDs
 request_num = 90  
 request_data = parse_request_data(logfile, request_ids, request_num)
-plot_request_data(request_data, 'tokenviz_'+logfile.split('.log')[0]+'.png')
+plot_request_data(request_data, 'tokenviz_'+logfile.split('.log')[0]+'.png')'''
 
-'''
-logfile = 'offline solver.log'
-request_counts = parse_request_num(logfile)
-plot_request_counts(request_counts, 'pendingviz.png')'''
+logfile = 'random.log'
+request_counts = parse_request_num(logfile, 300)
+plot_request_counts(request_counts, 'pendingviz.png')
 
