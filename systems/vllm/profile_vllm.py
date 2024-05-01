36a57299a6feb752cb1ca599f762cb9f5154f7d5
李佳蔚 <lijiawei@lijiaweis-Mac-mini.local>
2024-05-01 08:29:38 +0800

    measure vllm inference time

--- systems/vllm/profile_vllm.py
+++ systems/vllm/profile_vllm.py
@@ -1,6 +1,7 @@
 from modelscope.hub.api import HubApi
 from vllm import LLM, SamplingParams
 import os
+import datetime
 
 access_token = os.getenv('ACCESS_TOKEN')
 if access_token is None:
@@ -15,12 +16,18 @@ prompts = [
     "The capital of France is",
     "The future of AI is",
 ]
+deadlines = {}
+for prompt in prompts:
+    deadlines[prompt] = 2
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 llm = LLM(model="facebook/opt-125m")
 
 outputs = llm.generate(prompts, sampling_params)
 
 for output in outputs:
+    start = datetime.datetime.now()
     prompt = output.prompt
     generated_text = output.outputs[0].text
+    end = datetime.datetime.now()
+    print(end-start)
     print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
