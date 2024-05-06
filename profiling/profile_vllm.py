f46a076e260b586ee3f75699bf8eb05cd565bfa2
lijiawei20161002 <1346716237@qq.com>
2024-05-06 14:32:30 +0000

    measure latency

--- profiling/profile_vllm.py
+++ profiling/profile_vllm.py
@@ -12,23 +12,14 @@ api.login(access_token)
 from vllm import LLM, SamplingParams
 
 prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
+    f"{i+1}. {prompt}" for i, prompt in enumerate([
+        "Hello, my name is",
+        "The president of the United States is",
+        "The capital of France is",
+        "The future of AI is"
+    ] * 25)
 ]
+
 deadlines = {}
 for prompt in prompts:
     deadlines[prompt] = 2
@@ -36,10 +27,8 @@ sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 llm = LLM(model="facebook/opt-125m")
 
 outputs = llm.generate(prompts, sampling_params)
-start = datetime.datetime.now()
 for output in outputs:
     prompt = output.prompt
     generated_text = output.outputs[0].text
-    end = datetime.datetime.now()
-    print(end-start)
+    print(output.metrics.finished_time)
     #print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
